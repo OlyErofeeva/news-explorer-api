103d25ab616ceac43b958d404734c428d30882ca
Oly Erofeeva <o.leland@yandex.ru>
2021-01-16 01:46:07 +0300

    app

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const routes = require('./routers/index');
+const NotFoundError = require('./errors/not-found-error');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(routes);
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден'));
+});
+
+app.listen(PORT);
